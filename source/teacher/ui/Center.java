c3834759137bb96658e1e20132d7d8e65b346868
boldogzb <boldohaki@gmail.com>
2015-05-25 13:21:12 +0800

    daalgaar shalgax hesgiin interface

---
+++ source/teacher/ui/Center.java
@@ -0,0 +1,132 @@
+package homeworkcheckview;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxListCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Center {
+
+    public static final ObservableList<FlowPane> oyutan = FXCollections.observableArrayList();
+    public static final ObservableList<FlowPane> daalgavar = FXCollections.observableArrayList();
+    public static final ObservableList<FlowPane> hugatsaa = FXCollections.observableArrayList();
+    public static final ObservableList<FlowPane> test = FXCollections.observableArrayList();
+
+    private FlowPane root;
+
+    public Center() {
+
+        ListView listView = new ListView(oyutan);
+        listView.setEditable(false);
+        listView.setPrefSize(250, 700);
+        listView.setId("list-view");
+        
+
+        ListView listView2 = new ListView(daalgavar);
+        listView2.setEditable(false);
+        listView2.setPrefSize(250, 700);
+        listView2.setId("list-view");
+
+        ListView listView3 = new ListView(hugatsaa);
+        listView3.setEditable(false);
+        listView3.setPrefSize(250, 700);
+        listView3.setId("list-view");
+        
+        
+        
+        
+        FlowPane rows1 = new FlowPane();
+        rows1.setPrefSize(30, 30);
+        rows1.getChildren().addAll(new Label("НЭРС"));
+        rows1.setPrefWidth(50);
+        rows1.setAlignment(Pos.CENTER);
+        rows1.setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66; -fx-border-radius: 3px");
+        oyutan.add(rows1);
+        
+        FlowPane[] rows = new FlowPane[21];
+        for (int i = 0; i < rows.length; i++) {
+            rows[i] = new FlowPane();
+            rows[i].getChildren().add(new Label(""+i+" Оюутан"));
+            rows[i].setPrefWidth(50);
+            rows[i].setAlignment(Pos.CENTER);
+            rows[i].setPadding(new Insets(3, 3, 3, 3));
+            rows[i].setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66; -fx-border-radius: 3px;");
+            oyutan.add(rows[i]);
+        }
+      
+        
+        FlowPane spice1 = new FlowPane();
+        spice1.setPrefSize(30, 30);
+        spice1.getChildren().addAll(new Label("ДААЛГАВАР"));
+        spice1.setAlignment(Pos.CENTER);
+        spice1.setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66; -fx-border-radius: 3px;");
+        daalgavar.add(spice1);
+        
+          
+        FlowPane[] spice = new FlowPane[21];
+        for (int i = 0; i < spice.length; i++) {
+            
+            ImageView img1 = new ImageView(new Image("homeworkcheckview/icon.jpg"));
+            img1.setFitWidth(15);
+            img1.setFitHeight(15);
+            
+            
+            spice[i] = new FlowPane();
+            spice[i].getChildren().addAll(new Label("Тооны машин"), img1);
+            spice[i].setPrefWidth(50);
+            spice[i].setAlignment(Pos.CENTER);
+            spice[i].setPadding(new Insets(3, 3, 3, 3));
+            spice[i].setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66; -fx-border-radius: 3px;");
+            daalgavar.add(spice[i]);
+        }
+   
+        
+        FlowPane row1 = new FlowPane();
+        row1.setPrefSize(30, 30);
+        row1.getChildren().addAll(new Label("ХУГАЦАА"));
+        row1.setAlignment(Pos.CENTER);
+        row1.setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66;");
+        hugatsaa.add(row1);
+         
+        FlowPane[] row = new FlowPane[21];
+        for (int i = 0; i < row.length; i++) {
+            row[i] = new FlowPane();
+            row[i].getChildren().add(new Label("2015-05-15"));
+            row[i].setPrefWidth(50);
+            row[i].setAlignment(Pos.CENTER);
+            row[i].setPadding(new Insets(3, 3, 3, 3));
+            row[i].setStyle("-fx-background-color: #E6E6E6; -fx-border-width: 0px 7px 0px 7px; -fx-border-color: #99CC66; -fx-border-radius: 3px;");
+            hugatsaa.add(row[i]);
+        }
+        
+  
+        root = new FlowPane();
+        
+        Label gap1 = new Label("");
+        gap1.setPrefSize(15, 15);
+        
+       
+        
+        
+        root.setPadding(new Insets(30, 0, 0, 0));
+        root.getChildren().addAll(listView, gap1, listView2, listView3);
+        root.setPrefSize(300, 708);
+        root.setStyle("-fx-background-color: white;");
+        
+        
+    }
+
+    public FlowPane getContainer() {
+        return this.root;
+    }
+}
--- source/teacher/ui/homework.java
+++ source/teacher/ui/homework.java
@@ -1,46 +1,142 @@
-package surgaltin.alba;
+package homeworkcheckview;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Font;
 
 public class homework {
     
-    private FlowPane rightFp;
+    private FlowPane leftFp;
+    private ScrollPane scp;
     
     public homework(){
         
-        rightFp = new FlowPane(20,20);
-        rightFp.setPrefSize(250, 708);
-        rightFp.setStyle("-fx-background-color: #F6F6F7");
-        rightFp.setAlignment(Pos.CENTER);
+        leftFp = new FlowPane(20,20);
+        leftFp.setPrefSize(250, 708);
+        leftFp.setStyle("-fx-background-color: #E6E6E6");
+        leftFp.setAlignment(Pos.CENTER);
          
-        Label nameLbl = new Label("Ирсэн даалгавар");
+        Label nameLbl = new Label("Ирсэн Даалгаврууд");
         nameLbl.setAlignment(Pos.CENTER);
         nameLbl.setPrefSize(250, 60);
         nameLbl.setFont(javafx.scene.text.Font.font("Arial", 18));
         nameLbl.setStyle("-fx-background-color: #E6E6E6; -fx-padding: 10px;");
-        nameLbl.setId("text");
-  
-        FlowPane textFp = new FlowPane(10,5);
+    
+       
+        FlowPane homeworkFp = new FlowPane();
+        
+      
+        
+      
+        
+        homeworkFp.setPrefSize(220, 500);
+        Label lbl = new Label("Зураг Зурдаг Программ");
+        lbl.setFont(new Font("Arial", 13));
+        lbl.setPadding(new Insets(20, 20, 0, 40));
+        lbl.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            lbl.setStyle("-fx-text-fill:black;");
+        });
+        lbl.setOnMouseMoved(ae -> {
+            lbl.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+        
+        Label lbl1 = new Label("Машин Бүртэглийн Программ");
+        lbl1.setFont(new Font("Arial", 13));
+        lbl1.setPadding(new Insets(20, 20, 0, 20));
+         lbl1.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            lbl1.setStyle("-fx-text-fill:black;");
+        });
+        lbl1.setOnMouseMoved(ae -> {
+            lbl1.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+     
+        Label lbl2 = new Label("Фитнэс Бүртэглийн Программ");
+        lbl2.setFont(new Font("Arial", 13));
+        lbl2.setPadding(new Insets(20, 20, 0, 20));
+         lbl2.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            lbl2.setStyle("-fx-text-fill:black;");
+        });
+        lbl2.setOnMouseMoved(ae -> {
+            lbl2.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+        homeworkFp.getChildren().addAll(lbl,lbl1,lbl2);
+        homeworkFp.setId("panel");
+        
+        
+        
+        
+        
+        FlowPane textFp = new FlowPane(5,5);
         textFp.setPrefSize(200, 150);
-        
-        
-        Label irtsLb = new Label("Даалгавар шалгах");
+        Label irtsLb = new Label("Даалгавар Илгээх");
         irtsLb.setPrefSize(200, 20);
         irtsLb.setAlignment(Pos.CENTER);
-        irtsLb.setId("btns");
-        
-        Label dunLb = new Label("Даалгавар илгээх");
+         irtsLb.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            irtsLb.setStyle("-fx-text-fill:black;");
+        });
+        irtsLb.setOnMouseMoved(ae -> {
+            irtsLb.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+        Label dunLb = new Label("Даалгавар Шалгах");
         dunLb.setPrefSize(200, 20);
         dunLb.setAlignment(Pos.CENTER);
-        dunLb.setId("btns");
+         dunLb.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            dunLb.setStyle("-fx-text-fill:black;");
+        });
+        dunLb.setOnMouseMoved(ae -> {
+            dunLb.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+        Label idevhLb = new Label("Бүх Даалгавар Харах");
+        idevhLb.setPrefSize(200, 20);
+        idevhLb.setAlignment(Pos.CENTER);
+         idevhLb.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            idevhLb.setStyle("-fx-text-fill:black;");
+        });
+        idevhLb.setOnMouseMoved(ae -> {
+            idevhLb.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
+        Label allLb = new Label("Даалгаврийн оноо");
+        allLb.setPrefSize(200, 20);
+        allLb.setAlignment(Pos.CENTER);
+         allLb.setOnMouseExited(ae -> {
+            leftFp.setCursor(Cursor.DEFAULT);
+            allLb.setStyle("-fx-text-fill:black;");
+        });
+        allLb.setOnMouseMoved(ae -> {
+            allLb.setStyle("-fx-text-fill:Green;");
+            leftFp.setCursor(Cursor.HAND);
+        });
         
-        rightFp.setId("border");
-        textFp.getChildren().addAll(irtsLb,dunLb);
-        rightFp.getChildren().addAll(nameLbl, new inner_homework().getContainer(), textFp);
+        textFp.getChildren().addAll(irtsLb,dunLb,idevhLb,allLb);
+        leftFp.getChildren().addAll(nameLbl, homeworkFp, textFp);
+        
+        scp = new ScrollPane(leftFp);
+        
     }
-    public FlowPane getContainer() {
-        return this.rightFp;
+    public ScrollPane getContainer() {
+        return this.scp;
     }
+      
 }
